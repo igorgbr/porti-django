2410810bfcfc309049ce5696382807773b5022f2
igorgbr <igorgphotoarte@gmail.com>
2021-04-06 09:34:26 -0300

    first commit

---
+++ jobs/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Job
+
+
+def nick(request):
+    jobs = Job.objects
+    return render(request, "jobs/home.html", {"jobs": jobs})
+
+
+def detail(request, job_id):
+    job_detail = get_object_or_404(Job, pk=job_id)
+    return render(request, "jobs/detail.html", {"job": job_detail})
---
+++ portifolio/urls.py
@@ -0,0 +1,33 @@
+"""portifolio URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+import jobs.views
+from django.conf import settings
+
+import portifolio.settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", jobs.views.nick, name="home"),
+    path('jobs/<int:job_id>', jobs.views.detail, name='detail'),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(
+    portifolio.settings.MEDIA_URL, document_root=portifolio.settings.MEDIA_ROOT
+)
